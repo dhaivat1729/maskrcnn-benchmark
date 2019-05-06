d1203a4574d90ef24e2b9fafd0b8ebc43076d8ff
dhaivat666 <dhaivat1994@gmail.com>
2019-05-06 03:54:47 -0400

    For debug

--- maskrcnn_benchmark/data/build.py
+++ maskrcnn_benchmark/data/build.py
@@ -30,6 +30,7 @@ def build_dataset(dataset_list, transforms, dataset_catalog, is_train=True):
         )
     datasets = []
     for dataset_name in dataset_list:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", dataset_name, "!!!!!!!!!!!!!!!!!!!!!!!")
         data = dataset_catalog.get(dataset_name)
         factory = getattr(D, data["factory"])
         args = data["args"]
